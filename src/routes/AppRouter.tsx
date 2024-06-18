4578e1f2409abaa27ce6577a0749707229f330f6
Wilhen Gutierrez <w.gutierrez1206@gmail.com>
2024-06-18 18:02:49 -0500

    style: adjust project layout styles

--- src/routes/AppRouter.tsx
+++ src/routes/AppRouter.tsx
@@ -1,22 +1,79 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { ProtectedDashboardRoutes, ProtectedRoutes, PublicRoutes } from "."
-import { HomePage, LoginPage, NotFoundPage, RegisterPage } from "../pages"
+import { Navigate, Route, Routes } from "react-router-dom";
+// Components
+import { ProtectedDashboardRoutes, ProtectedRoutes, PublicRoutes } from ".";
+// Pages
+import {
+    HomePage,
+    LoginPage,
+    NotFoundPage
+} from "../pages";
+// Assets - icons
+import FileTextIcon from './../assets/icons/icon-file-text.svg'
+import FolderIcon from './../assets/icons/icon-folder.svg'
+import ChartIcon from './../assets/icons/icon-chart.svg'
+import UserGroupIcon from './../assets/icons/icon-users-group.svg'
+
+
+export const routes = [
+    {
+        path: "request",
+        element: <HomePage />,
+        label: "Solicitudes", 
+        icon: FileTextIcon,
+        permissions: []
+    },
+    {
+        path: "request-management",
+        element: <NotFoundPage />, 
+        label: "Gestión de recursos",
+        icon: FolderIcon,
+        permissions: []
+    },
+    {
+        path: "reports",
+        element: <NotFoundPage />, 
+        label: "Reportes",
+        icon: ChartIcon,
+        permissions:[]
+    },
+    {
+        path: "administrator",
+        element: <NotFoundPage />, 
+        label: "Administrador",
+        icon: UserGroupIcon,
+        permissions:[]
+    },
+];
+
+const protectedRoutes = [
+    {
+        path: "",
+        element: <Navigate to="/request" />,
+    },
+    ...routes.map((route, index) => ({
+        path: route.path,
+        element: route.element,
+        key: index,
+    })),
+];
 
 export const AppRouter = () => {
-    return (<Routes>
-        <Route path="/" element={<PublicRoutes />} >
-            <Route path="" element={<Navigate to="/login" />} />
-            <Route path="login" element={<LoginPage />} />
-            <Route path="register" element={<RegisterPage />} />
-        </Route>
-        <Route path="/" element={<ProtectedRoutes />} >
-            <Route path="" element={<ProtectedDashboardRoutes />} >
-                <Route path="" element={<Navigate to="/home" />} />
-                <Route path='home' element={<HomePage />} />
+    return (
+        <Routes>
+            {/* <Route path="" element={<Navigate to="/" />} /> */}
+            <Route path="" element={<PublicRoutes />}>
+                <Route path="" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<LoginPage />} />
             </Route>
-        </Route>
-        <Route path="/not-found" element={<NotFoundPage />} />
-        <Route path="*" element={<Navigate replace to="/not-found" />} />
-    </Routes>
-    )
-}
+            <Route path="" element={<ProtectedRoutes />}>
+                <Route path="" element={<ProtectedDashboardRoutes />}>
+                    {protectedRoutes.map((route, index) => (
+                        <Route key={index} path={route.path} element={route.element} />
+                    ))}
+                </Route>
+            </Route>
+            <Route path="/not-found" element={<NotFoundPage />} />
+            <Route path="*" element={<Navigate replace to="/not-found" />} />
+        </Routes>
+    );
+};
--- src/routes/PublicRoutes.tsx
+++ src/routes/PublicRoutes.tsx
@@ -3,11 +3,11 @@ import { useAppSelector } from "../redux/store/hooks";
 import { PublicLayout } from "../components";
 
 export const PublicRoutes = () => {
-    let auth = useAppSelector(state => state.auth)
-    let location = useLocation();
+    const auth = useAppSelector(state => state.auth)
+    const location = useLocation();
 
     return (auth.isAuth)
-        ? <Navigate to="/home" state={{ from: location }} replace />
+        ? <Navigate to="/request" state={{ from: location }} replace />
         : <PublicLayout>
             <Outlet />
         </PublicLayout>
